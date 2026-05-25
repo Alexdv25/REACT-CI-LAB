2216500b6351bb63ae9b9204a3bbf2d5d9f09701
Alex.dv <alexdv@gmail.com>
2026-05-25 23:14:08 +0300

    Break the pipeline intentionally

--- example-react-app-main/src/App.test.jsx
+++ example-react-app-main/src/App.test.jsx
@@ -17,3 +17,7 @@ describe('App', () => {
     expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument()
   })
 })
+
+test("This test should fail intentionally", () => {
+  expect(true).toBe(false);
+});
